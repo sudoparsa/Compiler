1c7b7a7b722012c37d852514117b0604338c6158
sudoparsa <sp.hoseiny@gmail.com>
2021-10-23 20:01:59 +0330

    clean code

--- compiler.py
+++ compiler.py
@@ -1,20 +1,21 @@
 SYMBOL = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<']
 WHITESPACE = [' ', '\r', '\t', '\v', '\f']
 EOF = ''
-DFA = {            #0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19
-    'letter':       [1,     1,0,    -2,0,0,0,   9,0,0,  11,0,   -5, 13, 13,0,   16],
-    'digit':        [3,     1,0,    3,0,0,0,    9,0,0,  11,0,   -5, 13, 13,0,   16],
-    'symbol':       [6,     2,0,    4,0,0,0,    9,0,0,  11,0,   -5, 13, 13,0,   16],
-    'whitespace':   [5,     2,0,    4,0,0,0,    9,0,0,  11,0,   -5, 13, 13,0,   16],
-    '\n':           [5,     2,0,    4,0,0,0,    9,0,0,  11,0,   -5, 13, 13,0,   15],
-    '/':            [12,    2,0,    4,0,0,0,    9,0,0,  -3,0,   16, 13, 15,0,   16],
-    '=':            [7,     2,0,    4,0,0,0,    8,0,0,  11,0,   -5, 13, 13,0,   16],
-    '*':            [10,    2,0,    4,0,0,0,    9,0,0,  11,0,   13, 14, 14,0,   16],
-    EOF:            [5,     2,0,    4,0,0,0,    9,0,0,  11,0,   -5, -4, -4,0,   15],
-    'invalid':      [-1,    -1,0,   -2,0,0,0,   -1,0,0, -1,0,   -1, 13, 13,0,   16]
+DFA = {  # 0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19
+    'letter': [1, 1, 0, -2, 0, 0, 0, 9, 0, 0, 11, 0, -5, 13, 13, 0, 16],
+    'digit': [3, 1, 0, 3, 0, 0, 0, 9, 0, 0, 11, 0, -5, 13, 13, 0, 16],
+    'symbol': [6, 2, 0, 4, 0, 0, 0, 9, 0, 0, 11, 0, -5, 13, 13, 0, 16],
+    'whitespace': [5, 2, 0, 4, 0, 0, 0, 9, 0, 0, 11, 0, -5, 13, 13, 0, 16],
+    '\n': [5, 2, 0, 4, 0, 0, 0, 9, 0, 0, 11, 0, -5, 13, 13, 0, 15],
+    '/': [12, 2, 0, 4, 0, 0, 0, 9, 0, 0, -3, 0, 16, 13, 15, 0, 16],
+    '=': [7, 2, 0, 4, 0, 0, 0, 8, 0, 0, 11, 0, -5, 13, 13, 0, 16],
+    '*': [10, 2, 0, 4, 0, 0, 0, 9, 0, 0, 11, 0, 13, 14, 14, 0, 16],
+    EOF: [5, 2, 0, 4, 0, 0, 0, 9, 0, 0, 11, 0, -5, -4, -4, 0, 15],
+    'invalid': [-1, -1, 0, -2, 0, 0, 0, -1, 0, 0, -1, 0, -1, 13, 13, 0, 16]
 }
 GOAL_STATES = [2, 4, 5, 6, 8, 9, 11, 15]
 LOOK_AHEAD_STATES = [2, 4, 9, 11, -5]
+
 
 def transit(current_state, character):
     if character.isalpha():
@@ -29,29 +30,38 @@ def transit(current_state, character):
         next_state = DFA[character][current_state]
     else:
         next_state = DFA['invalid'][current_state]
-    
+
     return next_state
+
 
 tokens = {}
 errors = {}
+
+
 def add_token(lineno, token):
     if lineno in tokens:
         tokens[lineno].append(token)
     else:
         tokens[lineno] = [token]
+
+
 def add_error(lineno, error):
     if lineno in errors:
         errors[lineno].append(error)
     else:
         errors[lineno] = [error]
+
+
 SYMBOL_TABLE = ['', 'if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return']
+
+
 def get_token(state, lexeme):
     if state == 2:
         if lexeme in SYMBOL_TABLE and SYMBOL_TABLE.index(lexeme) <= 8:
-                return 'KEYWORD', lexeme
+            return 'KEYWORD', lexeme
         else:
             if not lexeme in SYMBOL_TABLE:
-                    SYMBOL_TABLE.append(lexeme)
+                SYMBOL_TABLE.append(lexeme)
             return 'ID', lexeme
     if state == 4:
         return 'NUM', lexeme
@@ -60,7 +70,8 @@ def get_token(state, lexeme):
     if state == 6 or state == 8 or state == 9 or state == 11:
         return 'SYMBOL', lexeme
     if state == 15:
-        return None 
+        return None
+
 
 def get_error(state, lexeme):
     if state == -1 or state == -5:
@@ -84,12 +95,11 @@ while cursor < len(inp):
     state = 0
     temp_lineno = lineno
     while temp <= len(inp):
-        
+
         if temp == len(inp):
             character = EOF
         else:
             character = inp[temp]
-
 
         state = transit(state, character)
         if state in LOOK_AHEAD_STATES:
@@ -102,13 +112,13 @@ while cursor < len(inp):
             if not token == None:
                 add_token(lineno, token)
             cursor = temp + 1
-            lineno = temp_lineno ## ?????
+            lineno = temp_lineno  ## ?????
             break
         if state < 0:
             error = get_error(state, inp[cursor:temp + 1])
-            add_error(lineno, error) 
+            add_error(lineno, error)
             cursor = temp + 1
-            lineno = temp_lineno ## ?????
+            lineno = temp_lineno  ## ?????
             break
         temp += 1
 
@@ -135,5 +145,3 @@ else:
             f.write(f'({token[0]}, {token[1]}) ')
         f.write('\n')
 f.close()
-
-
